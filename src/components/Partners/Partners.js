ea40c8357e2fa963b20fb754f60ddcf095abb00b
mnlght03 <evmorvag@gmail.com>
2023-10-21 22:06:48 +0700

    doubled partners cards

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,53 +1,58 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import Section from '../ui/Section';
 import {bigTextDesktop, mediumTextMobile} from '../../utils';
 import PartnersButton from './PartnersButton';
-import { GlobalContext } from '../../context';
+import {GlobalContext} from '../../context';
 import PartnersSlider from './PartnersSlider';
 import PartnerService from '../../services/PartnerService';
 import PartnersSliderMobile from "./PartnersSliderMobile";
 
 export default function Partners() {
-  const { isDesktop } = useContext(GlobalContext);
-  const [partners, setPartners] = useState([]);
+    const {isDesktop} = useContext(GlobalContext);
+    const [partners, setPartners] = useState([]);
 
-  const fetchPartners = async () => {
-    const partners = await PartnerService.getAll();
-    setPartners(partners);
-  };
+    const fetchPartners = async () => {
+        const partners = await PartnerService.getAll();
+        setPartners(partners);
+    };
 
-  useEffect(() => {
-    fetchPartners();
-  }, []);
+    useEffect(() => {
+        fetchPartners();
+    }, []);
 
-  const partnersSplit = useMemo(() => {
-    return [
-      partners.slice(0, Math.floor(partners.length / 2)),
-      partners.slice(Math.floor(partners.length / 2)),
-    ];
-  }, [partners]);
+    const partnersSplit = useMemo(() => {
+        const leftSplit = partners.slice(0, Math.floor(partners.length / 2));
+        const rightSplit = partners.slice(Math.floor(partners.length / 2));
 
-  return (
-    <Section bgColor="bg-black" id="partners">
-      <div className="flex items-center justify-between mt-6 xl:mt-8">
-        <h1 className={`text-white font-semibold ${mediumTextMobile} ${bigTextDesktop}`}>
-          С нами сотрудничали
-        </h1>
-        {isDesktop && <PartnersButton />}
-      </div>
-      {isDesktop
-          ? (
-              <>
-                <PartnersSlider
-                    partners={partnersSplit[0]}
-                    classes="mt-16"
-                />
-                <PartnersSlider partners={partnersSplit[1]} classes='mt-4'/>
-              </>
-          )
-          : (<PartnersSliderMobile partners={partners} classes='mt-4 mb-4' />)
-      }
+        return [
+            [...leftSplit, ...leftSplit],
+            [...rightSplit, ...rightSplit]
+        ];
+    }, [partners]);
 
-    </Section>
-  );
+    return (
+        <Section bgColor="bg-black" id="partners">
+            <div className="flex items-center justify-between mt-6 xl:mt-8">
+                <h1 className={`text-white font-semibold ${mediumTextMobile} ${bigTextDesktop}`}>
+                    С нами сотрудничали
+                </h1>
+                {isDesktop && <PartnersButton/>}
+            </div>
+            {isDesktop
+                ? (
+                    <>
+                        <PartnersSlider
+                            partners={partnersSplit[0]}
+                            classes="mt-16"
+                        />
+                        <PartnersSlider partners={partnersSplit[1]}
+                                        classes='mt-4'/>
+                    </>
+                )
+                : (<PartnersSliderMobile partners={partners}
+                                         classes='mt-4 mb-4'/>)
+            }
+
+        </Section>
+    );
 }
