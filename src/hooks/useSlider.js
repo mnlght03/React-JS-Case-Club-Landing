ac55c0d15fb9340c94ddfb80a9df1363f4bf5f68
mnlght03 <evmorvag@gmail.com>
2023-10-21 19:47:33 +0700

    added mobile slider acceleration

--- src/hooks/useSlider.js
+++ src/hooks/useSlider.js
@@ -3,24 +3,38 @@ import {useState} from "react";
 export function useSlider(minOffset, maxOffset) {
     const [offset, setOffset] = useState(minOffset);
     const [startX, setStartX] = useState(0);
+    const [clientX, setClientX] = useState(0)
+    const [startTime, setStartTime] = useState(0);
+
+    const SCROLL_SPEED_MULTIPLIER = 1;
+    const SCROLL_ACCELERATION_MULTIPLIER = 100;
 
     const handleTouchStart = (event) => {
-        // event.target.closest('.slider').style.transition = 'none';
+        event.target.closest('.slider').style.transition = 'none';
         setStartX(event.targetTouches[0].clientX);
+        setClientX(event.targetTouches[0].clientX);
+        setStartTime(event.timeStamp);
     }
 
-    const SCROLL_SPEED_MULTIPLIER = 1.52;
 
     const handleTouchMove = (event) => {
-        const clientX = event.targetTouches[0].clientX;
-        setOffset(offset + (clientX - startX) * SCROLL_SPEED_MULTIPLIER);
-        setStartX(clientX);
+        setOffset(offset + (event.targetTouches[0].clientX - clientX) * SCROLL_SPEED_MULTIPLIER);
+        setClientX(event.targetTouches[0].clientX);
     }
 
     const handleTouchEnd = (event) => {
         event.target.closest('.slider').style.transition = 'transform 0.2s ease-out';
-        if (offset > minOffset) setOffset(minOffset);
-        if (offset < -maxOffset) setOffset(-maxOffset);
+
+        const time = event.timeStamp - startTime;
+
+        const delta = clientX - startX;
+
+        const newOffset = offset + delta * SCROLL_ACCELERATION_MULTIPLIER / time;
+
+        setOffset(newOffset);
+
+        if (newOffset > minOffset) setOffset(minOffset);
+        if (newOffset < -maxOffset) setOffset(-maxOffset);
     }
 
     return {
